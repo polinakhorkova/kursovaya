dc9878d1ccdc302c94edb2150ea9a5a48f4d12bb
polinahmedova <polinahmedova510@gmail.com>
2024-01-06 17:46:22 +0500

    22

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -39,117 +39,117 @@ class HomeView extends GetView<HomeController> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                     Padding(
-                        padding: EdgeInsets.only(left: 100),
-                        child:
-                    ElevatedButton(
-                      onPressed: () => Get.toNamed(Routes.INSTRUCTIONS),
-                      child: Container(
-                        width: 250,
-                        padding: EdgeInsets.all(15),
-                        child: Center(
-                          child: Text(
-                            'Инструкции',
-                            style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.w800),
+                    Padding(
+                      padding: EdgeInsets.only(left: 100),
+                      child: ElevatedButton(
+                        onPressed: () => Get.toNamed(Routes.INSTRUCTIONS),
+                        child: Container(
+                          width: 250,
+                          padding: EdgeInsets.all(15),
+                          child: Center(
+                            child: Text(
+                              'Инструкции',
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.w800),
+                            ),
                           ),
                         ),
-                      ), 
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromRGBO(178, 124, 232, 1)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color.fromRGBO(178, 124, 232, 1)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
                         ),
                       ),
-                    ),),
+                    ),
                     SizedBox(height: 20),
-                     Padding(
-                        padding: EdgeInsets.only(left: 100),
-                        child:
-                    ElevatedButton(
-                      onPressed: () => Get.toNamed(Routes.RECOMMENDS),
-                      child: Container(
-                        width: 250,
-                        padding: EdgeInsets.all(15),
-                        child: Center(
-                          child: Text(
-                            'Рекомендации',
-                            style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.w800),
+                    Padding(
+                      padding: EdgeInsets.only(left: 100),
+                      child: ElevatedButton(
+                        onPressed: () => Get.toNamed(Routes.RECOMMENDS),
+                        child: Container(
+                          width: 250,
+                          padding: EdgeInsets.all(15),
+                          child: Center(
+                            child: Text(
+                              'Рекомендации',
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.w800),
+                            ),
+                          ),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color.fromRGBO(178, 124, 232, 1)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
                         ),
                       ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromRGBO(178, 124, 232, 1)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ),),
+                    ),
                     SizedBox(height: 20),
-                     Padding(
-                        padding: EdgeInsets.only(left: 100),
-                        child:
-                    ElevatedButton(
-                      onPressed: () => Get.toNamed(Routes.DEMO),
-                      child: Container(
-                        width: 250,
-                        padding: EdgeInsets.all(15),
-                        child: Center(
-                          child: Text(
-                            'Демо',
-                            style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.w800),
+                    Padding(
+                      padding: EdgeInsets.only(left: 100),
+                      child: ElevatedButton(
+                        onPressed: () => Get.toNamed(Routes.DEMO),
+                        child: Container(
+                          width: 250,
+                          padding: EdgeInsets.all(15),
+                          child: Center(
+                            child: Text(
+                              'Демо',
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.w800),
+                            ),
+                          ),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color.fromRGBO(178, 124, 232, 1)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
                         ),
                       ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromRGBO(178, 124, 232, 1)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ),),
+                    ),
                     SizedBox(height: 20),
-                     Padding(
-                        padding: EdgeInsets.only(left: 100),
-                        child:
-                    ElevatedButton(
-                      onPressed: () => Get.toNamed(Routes.INSTRUMENTS),
-                      child: Container(
-                        width: 250,
-                        padding: EdgeInsets.all(15),
-                        child: Center(
-                          child: Text(
-                            'Инструменты',
-                            style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.w800),
+                    Padding(
+                      padding: EdgeInsets.only(left: 100),
+                      child: ElevatedButton(
+                        onPressed: () => Get.toNamed(Routes.INSTRUMENTS),
+                        child: Container(
+                          width: 250,
+                          padding: EdgeInsets.all(15),
+                          child: Center(
+                            child: Text(
+                              'Инструменты',
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.w800),
+                            ),
+                          ),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color.fromRGBO(178, 124, 232, 1)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
                         ),
                       ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromRGBO(178, 124, 232, 1)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ),),
+                    ),
                   ],
                 ),
               ),
@@ -176,8 +176,7 @@ class HomeView extends GetView<HomeController> {
                       ),
                       Padding(
                         padding: EdgeInsets.only(top: 10, right: 150),
-                        child: Text(
-                            'Вы сможете подобрать приложения',
+                        child: Text('Вы сможете подобрать приложения',
                             style: TextStyle(
                                 fontSize: 30,
                                 fontWeight: FontWeight.w800,
@@ -185,8 +184,7 @@ class HomeView extends GetView<HomeController> {
                       ),
                       Padding(
                         padding: EdgeInsets.only(top: 10, right: 150),
-                        child: Text(
-                            'для разработки дизайна, который',
+                        child: Text('для разработки дизайна, который',
                             style: TextStyle(
                                 fontSize: 30,
                                 fontWeight: FontWeight.w800,
@@ -233,72 +231,106 @@ class HomeView extends GetView<HomeController> {
             ],
           ),
         ),
-        
-      
-        bottomNavigationBar:  Container(
-         // width: 100,
-         height: 150,
+        bottomNavigationBar: Container(
+          // width: 100,
+          height: 150,
           decoration: BoxDecoration(
             color: Color.fromRGBO(178, 124, 232, 1),
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              
               Column(
                 children: [
-              
-            Padding(padding: EdgeInsets.only(top: 20, left: 170, ),
-            
-            child: Text('Пермский Государственный Гуманитарно-Педагогический Университет',
-            style: TextStyle(fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),),),
-                                
-            Padding(padding: EdgeInsets.only(top: 10,left: 70, ),
-            
-            child: Text('Разработчики:',
-            style: TextStyle(fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),),),
-            Padding(padding: EdgeInsets.only(top: 10, left: 70, ),
-            
-            child: Text('Ахмедова П.С. ',
-            style: TextStyle(fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),),),
-             Padding(padding: EdgeInsets.only(top: 10, left: 70, ),
-            
-            child: Text('Хорькова П.А. ',
-            style: TextStyle(fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),),),
-                                
-                                
-                                ],
-                                
+                  SizedBox(width: 10),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 20,
+                      left: 170,
+                    ),
+                    child: Text(
+                      'Пермский Государственный Гуманитарно-Педагогический Университет',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 10,
+                      left: 70,
+                    ),
+                    child: Text(
+                      'Разработчики:',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 10,
+                      left: 70,
+                    ),
+                    child: Text(
+                      'Ахмедова П.С. ',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 10,
+                      left: 70,
+                    ),
+                    child: Text(
+                      'Хорькова П.А. ',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
-              Column(children: [
-                Padding(padding: EdgeInsets.only(top: 50, right: 700),
-            
-            child: Text('Контакты:',
-            style: TextStyle(fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),),),
-                                Padding(padding: EdgeInsets.only(top: 10, right: 700),
-            
-            child: Text('67026@pspu.ru',
-            style: TextStyle(fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),),),
-                                Padding(padding: EdgeInsets.only(top: 10, right: 700),
-            
-            child: Text('67038@pspu.ru',
-            style: TextStyle(fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),),),
-              ],),
-            
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 50, right: 700),
+                    child: Text(
+                      'Контакты:',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, right: 700),
+                    child: Text(
+                      '67026@pspu.ru',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, right: 700),
+                    child: Text(
+                      '67038@pspu.ru',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
